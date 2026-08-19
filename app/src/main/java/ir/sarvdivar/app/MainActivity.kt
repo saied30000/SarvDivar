50bc3078b71c30b05d5efc9c926a0c423fc2b140
saied30000 <35403514+saied30000@users.noreply.github.com>
2026-08-20 01:39:14 +0330

    Update MainActivity.kt

--- app/src/main/java/ir/sarvdivar/app/MainActivity.kt
+++ app/src/main/java/ir/sarvdivar/app/MainActivity.kt
@@ -1,14 +1,28 @@
 package ir.sarvdivar.app
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
+
+    private var filePathCallback: ValueCallback<Array<Uri>>? = null
+
+    companion object {
+        private const val FILE_CHOOSER_REQUEST_CODE = 1001
+    }
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,19 +35,103 @@ class MainActivity : AppCompatActivity() {
         webView.settings.databaseEnabled = true
         webView.settings.loadsImagesAutomatically = true
         webView.settings.setSupportZoom(false)
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
 
         webView.webViewClient = WebViewClient()
+
+        webView.webChromeClient = object : WebChromeClient() {
+
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePath: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+
+                filePathCallback?.onReceiveValue(null)
+                filePathCallback = filePath
+
+                return try {
+                    val intent = fileChooserParams?.createIntent()
+
+                    if (intent != null) {
+                        startActivityForResult(
+                            intent,
+                            FILE_CHOOSER_REQUEST_CODE
+                        )
+                        true
+                    } else {
+                        filePathCallback = null
+                        false
+                    }
+
+                } catch (e: Exception) {
+                    filePathCallback = null
+                    false
+                }
+            }
+        }
 
         webView.loadUrl("https://sarvdivar.ir/")
 
         setContentView(webView)
+
+        ViewCompat.setOnApplyWindowInsetsListener(webView) { view, insets ->
+            val systemBars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+            )
+
+            view.setPadding(
+                0,
+                systemBars.top,
+                0,
+                systemBars.bottom
+            )
+
+            insets
+        }
+
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (webView.canGoBack()) {
+                        webView.goBack()
+                    } else {
+                        finish()
+                    }
+                }
+            }
+        )
     }
 
-    override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == FILE_CHOOSER_REQUEST_CODE) {
+
+            val results: Array<Uri>? =
+                if (resultCode == Activity.RESULT_OK) {
+
+                    data?.clipData?.let { clipData ->
+                        Array(clipData.itemCount) { index ->
+                            clipData.getItemAt(index).uri
+                        }
+                    } ?: data?.data?.let {
+                        arrayOf(it)
+                    }
+
+                } else {
+                    null
+                }
+
+            filePathCallback?.onReceiveValue(results)
+            filePathCallback = null
         }
     }
 }
